cbd05db015e3636dc78110f57ffcb2c0ded72946
ezjing <god998877@naver.com>
2023-07-20 17:49:45 +0900

    react_0720(컴포넌트 합성, axio)

--- react2/src/day06/WelcomeDialog2.jsx
+++ react2/src/day06/WelcomeDialog2.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import FancyBorder from "./FancyBorder";
+import Dialog2 from "./Dialog2";
 
-// 부모 컴포넌트에서 자식 컴포넌트를 호출하여 UI 구성
-function WelcomeDialog(props) {
+function WelcomeDialog2(props) {
 
     return (
-        // 컴포넌트 합성 - 포함
-        // 자식 컴포넌트를 호출 시 자식 컴포넌트의 children 부분에 html 태그를 추가하여 전달할 수 있음
-        <div className={'border border-2 border-primary p-3'}>
-            <h3>부모 컴포넌트 영역</h3>
-            <FancyBorder color={'blue'}>    {/* 원래는 <FancyBorder/> 이런식으로 사용 했는데, html 태그 처럼 사용함 그러면 필요한 태그들을 자식태그에 추가해 줄 수 있다 */}
-                <h1 className={'Dialog-title'}>어서오세요.</h1>
-                <p className={'Dialog-message'}>우리 사이트에 방문하신 것을 환영합니다.</p>
-            </FancyBorder>
-        </div>
+        <div>
+            <Dialog2
+                title={'어서 오세요'}
+                message={'우리 사이트에 방문한 것을 환영합니다.'}
+            />
 
-    )
+            <Dialog2 title={'경고!!!'} message={'오류가 발생했습니다.'} />
+        </div>
+    );
 }
 
-export default WelcomeDialog;
+export default WelcomeDialog2;
